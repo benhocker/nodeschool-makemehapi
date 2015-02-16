d2d8f45d31595d74a9bffe357033ba33c7ec3575
Ben Hocker <ben.hocker@cat.com>
2015-02-16 17:17:13 -0600

    3/12

---
+++ 3.js
@@ -0,0 +1,26 @@
+var Hapi = require("hapi");
+var Path = require('path');
+var server = new Hapi.Server();
+
+function one(request, reply) {
+  reply("Hello Hapi");
+}
+
+function two(request, reply) {
+	reply("Hello " + request.params.name);
+}
+
+server.connection({
+	host: "localhost",
+	port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+	path: "/",
+	method: "GET",
+	handler: {
+		file: "index.html" // Path.join(__dirname, "index.html")
+	}
+});
+
+server.start();
